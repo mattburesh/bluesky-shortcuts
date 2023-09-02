ae7ea4e2672c36a836334471b1ef2f39bffed742
Matt Buresh <matt@bure.sh>
2023-09-01 20:47:33 -0500

    More work on auto verions incrementing

--- bin/bump-version.js
+++ bin/bump-version.js
@@ -8,5 +8,6 @@ let manifestJSON = JSON.parse(fs.readFileSync(fileName, 'utf8'))
 if (manifestJSON.version != __VERSION__) {
     console.log("Change in versions, updating manifest file...")
     manifestJSON.version = __VERSION__
-}
 
+    fs.writeFileSync(fileName, JSON.stringify(manifestJSON, null, 4))
+}
