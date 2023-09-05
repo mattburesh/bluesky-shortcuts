f18054af05f117965848c76e6fabd709628f410e
Matt Buresh <matt@bure.sh>
2023-09-04 19:56:41 -0500

    Update version

---
+++ src/help.js
@@ -0,0 +1,9 @@
+export class Help {
+
+    constructor() {
+
+    }
+
+    hideHelpModal() {}
+    renderHelpModal() {}
+}
---
+++ src/shortcuts.js
@@ -0,0 +1,11 @@
+import {Feed} from './feed.js'
+
+export const SHORTCUTS = {
+    KeyJ : {
+        function: Feed.moveToNextPost()
+    },
+    KeyK: {},
+    KeyL: {},
+    KeyR: {},
+    Enter: {}
+}
