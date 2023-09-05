a867c1c23eebfc47dd7b7f0cd650489eb6615663
Matt Buresh <matt@bure.sh>
2023-09-04 22:36:44 -0500

    Initial work on tab cycling

--- src/feed.js
+++ src/feed.js
@@ -1,65 +1,118 @@
 export class Feed {
-    
-    previousPost;
-    currentPost;
-    nextPost;
-    feedList = []
+    activeFeed
+    feeds
+    previousPost
+    currentPost
+    nextPost
+    feedListButtons = []
 
     constructor(postElements) {
         this.getPinnedFeeds()
+        this.feeds = document.querySelectorAll(
+            '[data-testid*="-feed-flatlist"]'
+        )
         this.nextPost = postElements.children[0]
+        console.log(this.nextPost)
     }
 
-    get previousPost() { return this.previousPost }
-    set previousPost(post) { this.previousPost = post }
+    get previousPost() {
+        return this.previousPost
+    }
+    set previousPost(post) {
+        this.previousPost = post
+    }
 
-    get currentPost() { return this.currentPost }
-    set currentPost(post) { this.currentPost = post }
+    get currentPost() {
+        return this.currentPost
+    }
+    set currentPost(post) {
+        this.currentPost = post
+    }
 
-    get nextPost() { return this.nextPost}
-    set nextPost(post) { this.nextPost = post }
+    get nextPost() {
+        return this.nextPost
+    }
+    set nextPost(post) {
+        this.nextPost = post
+    }
+
+    checkFeedStatus() {
+        this.feeds = document.querySelectorAll(
+            '[data-testid*="-feed-flatlist"]'
+        )
+    }
 
     cycleFeeds() {
-        let done = false 
-        for (let i = 0; i < this.feedList.length; i++) {
-            if (done === true) {
-                this.feedList[i].activetab = true
-                this.feedList[i].click()
+        let done = false
+
+        for (let i = 0; i < this.feeds.length; i++) {
+            // if (done === true) {
+
+            // }
+
+            if (i === this.feedListButtons.length - 1) {
+                this.feedListButtons[0].click()
+                // this.setActiveFeed(this.feeds[i].children[0])
+                console.log(this.feeds[i].childNodes[0].childNodes[0])
+                this.setActiveFeed(this.feeds[i].childNodes.item(0))
                 break
             }
 
-            if (this.feedList[i].activetab === true) {
-                this.feedList[i].activetab = false
-                done = true
-            }
-
-            if (i === this.feedList.length - 1) {
-                this.feedList[i].activetab = false
-                this.feedList[0].activetab = true
-                this.feedList[0].click()
+            if (this.feeds[i].offsetParent !== null) {
+                this.feedListButtons[i + 1].click()
+                // this.setActiveFeed(this.feeds[i + 1].children[0])
+                console.log(this.feeds[i + 1].childNodes[0].childNodes[0])
+                this.setActiveFeed(this.feeds[i + 1].childNodes.item(0))
                 break
             }
         }
+
+        // for (let i = 0; i < this.feedList.length; i++) {
+        //     if (done === true) {
+        //         this.feedList[i].activetab = true
+        //         this.feedList[i].click()
+        //         // this.rebuildActiveFeed(this.feedList[i])
+        //         break
+        //     }
+
+        //     if (this.feedList[i].activetab === true) {
+        //         this.feedList[i].activetab = false
+        //         done = true
+        //     }
+
+        //     if (i === this.feedList.length - 1) {
+        //         this.feedList[i].activetab = false
+        //         this.feedList[0].activetab = true
+        //         this.feedList[0].click()
+        //         this.rebuildActiveFeed(this.feedList[0])
+        //         break
+        //     }
+        // }
     }
 
     getPinnedFeeds() {
-        let feeds = document.querySelector('[data-testid="homeScreenFeedTabs"] > div > div').children
+        let feeds = document.querySelector(
+            '[data-testid="homeScreenFeedTabs"] > div > div'
+        ).children
 
         for (let item of feeds) {
             if (item.style.borderBottomColor === "rgb(0, 133, 255)") {
                 item.activetab = true
-            } 
-            this.feedList.push(item)
+            }
+            this.feedListButtons.push(item)
         }
     }
 
     likeToggleCurrentPost() {
-        let like = this.currentPost.querySelector('[aria-label*="Like ("') ?? this.currentPost.querySelector('[aria-label*="Unlike ("')
+        let like =
+            this.currentPost.querySelector('[aria-label*="Like ("') ??
+            this.currentPost.querySelector('[aria-label*="Unlike ("')
         like.click()
     }
 
     loadNewPosts() {
-        let loadPostsButton = document.querySelector('[aria-label*="Load new posts"') ?? null
+        let loadPostsButton =
+            document.querySelector('[aria-label*="Load new posts"') ?? null
         if (loadPostsButton) loadPostsButton.click()
     }
 
@@ -68,7 +121,11 @@ export class Feed {
         this.previousPost = this.currentPost.previousSibling ?? this.currentPost
         this.nextPost = this.currentPost.nextSibling
         this.toggleCurrentPostHighlight()
-        this.currentPost.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+        this.currentPost.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "nearest",
+        })
     }
 
     moveToPreviousPost() {
@@ -78,14 +135,18 @@ export class Feed {
         this.previousPost = this.currentPost.previousSibling ?? this.currentPost
         this.nextPost = this.currentPost.nextSibling
         this.toggleCurrentPostHighlight()
-        this.currentPost.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+        this.currentPost.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "nearest",
+        })
     }
 
     /**
      * Open the selected post
-     * 
+     *
      * The first instance of a url that matches the regex below should be the link for a given post
-     * 
+     *
      * Todo: Replies that show up in a feed would need to be handled differently
      */
     openCurrentPost() {
@@ -93,14 +154,16 @@ export class Feed {
 
         // here's a really bad regex that should probably be fixed
         const re = /^https:\/\/bsky.app\/profile\/.*\/post\/[a-zA-Z0-9]*$/
-        links.forEach(element => {
+        links.forEach((element) => {
             if (re.exec(element.href)) element.click()
-        });
+        })
     }
 
     quoteCurrentPost() {}
 
-    reloadFeed() {}
+    rebuildActiveFeed(feed) {
+        let test = document.querySelectorAll('[data-testid*="-feed-flatlist"]')
+    }
 
     replyToCurrentPost() {
         let reply = this.currentPost.querySelector('[aria-label*="Reply ("')
@@ -109,9 +172,25 @@ export class Feed {
 
     repostCurrentPost() {}
 
+    setActiveFeed(feed) {
+        this.previousPost = null
+        this.currentPost = null
+        this.nextPost = feed.childNodes.item(0)
+
+        // this.debugPostStatus()
+    }
+
     toggleCurrentPostHighlight() {
-        this.previousPost.style.removeProperty('border')
-        this.nextPost.style.removeProperty('border')
-        this.currentPost.style.border = '2px solid blue'
+        if (this.previousPost) this.previousPost.style.removeProperty("border")
+        if (this.nextPost) this.nextPost.style.removeProperty("border")
+        if (this.currentPost) this.currentPost.style.border = "2px solid blue"
+    }
+
+    debugPostStatus() {
+        console.log({
+            previousPost: this.previousPost,
+            currentPost: this.currentPost,
+            nextPost: this.nextPost,
+        })
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,47 +1,52 @@
-import {Feed} from './feed.js'
+import { Feed } from "./feed.js"
 
-console.log('bsky shortcuts running...')
+console.log("bsky shortcuts running...")
 
 let ready = false
 let postFeed = null
 
 // wait for the feed to load
-waitForElement('div[data-testid="followingFeedPage-feed-flatlist"] > div').then((element) => {
-    postFeed = new Feed(element)
-    ready = true
-})
+waitForElement('div[data-testid="followingFeedPage-feed-flatlist"] > div').then(
+    (element) => {
+        postFeed = new Feed(element)
+        ready = true
+    }
+)
 
 addEventListener("keydown", (event) => {
     // ignore shortcuts if the feed hasn't loaded yet
     if (ready === false) return false
 
     // ignore shortcuts if the reply/post window is open
-    if (document.getElementsByClassName("ProseMirror-focused").length > 0) return false
+    if (document.getElementsByClassName("ProseMirror-focused").length > 0)
+        return false
 
     // ignore shortcuts if a modifier key is being held down
-    if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) return false
+    if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey)
+        return false
 
-    if (event.code === 'KeyJ') postFeed.moveToNextPost()
-    if (event.code === 'KeyK') postFeed.moveToPreviousPost()
+    postFeed.checkFeedStatus()
 
-    if (event.code === 'KeyL') postFeed.likeToggleCurrentPost()
-    if (event.code === 'KeyR') postFeed.replyToCurrentPost()
-    if (event.code === 'KeyC') postFeed.cycleFeeds()
+    if (event.code === "KeyJ") postFeed.moveToNextPost()
+    if (event.code === "KeyK") postFeed.moveToPreviousPost()
+
+    if (event.code === "KeyL") postFeed.likeToggleCurrentPost()
+    if (event.code === "KeyR") postFeed.replyToCurrentPost()
+    if (event.code === "KeyC") postFeed.cycleFeeds()
     // if (event.code === 'KeyT') // repost / quote repost
-    // if (event.code === 'KeyN') // new post 
+    // if (event.code === 'KeyN') // new post
     // if (event.code === 'Slash') // show shortcuts
-    if (event.code === 'Enter') postFeed.openCurrentPost()
-    if (event.code === 'Period') postFeed.loadNewPosts()
-    
+    if (event.code === "Enter") postFeed.openCurrentPost()
+    if (event.code === "Period") postFeed.loadNewPosts()
 })
 
 function waitForElement(selector) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         if (document.querySelector(selector)) {
             return resolve(document.querySelector(selector))
         }
 
-        const observer = new MutationObserver(mutations => {
+        const observer = new MutationObserver((mutations) => {
             if (document.querySelector(selector)) {
                 observer.disconnect()
                 resolve(document.querySelector(selector))
@@ -50,7 +55,7 @@ function waitForElement(selector) {
 
         observer.observe(document.body, {
             childList: true,
-            subtree: true
+            subtree: true,
         })
     })
 }
@@ -63,8 +68,11 @@ function querySelectorAllLive(element, selector) {
     let observer = new MutationObserver((mutations) => {
         console.log(mutations)
         mutations.forEach((mutation) => {
-            [].forEach.call(mutation.addedNodes, (node) => {
-                if (node.nodeType === Node.ELEMENT_NODE && node.matches(selector)) {
+            ;[].forEach.call(mutation.addedNodes, (node) => {
+                if (
+                    node.nodeType === Node.ELEMENT_NODE &&
+                    node.matches(selector)
+                ) {
                     result.push(node)
                 }
             })
